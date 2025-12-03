613eae1b35e341284e8f58c369fa13c90b9139c6
paulflieshman <paulfury@gmail.com>
2025-12-03 15:42:16 +0000

    made the tests for present with Will navigating

--- tests/test_present.py
+++ tests/test_present.py
@@ -8,3 +8,32 @@ def test_wrapped_twice():
     # Call wrap method and add contents
     # Assert captured message is equal to expcected error message
     
+    present = Present()
+    present.wrap("Candy cane")
+    with pytest.raises(Exception) as e:
+        present.wrap("Candy cane")
+    error_message = str(e.value)
+    assert error_message == "A contents has already been wrapped."
+
+def test_unwrap_without_wrapping():
+    # Make a new present object
+    # Prepare to capture error message
+    # Call unwrap method 
+    # Assert captured message is equal to expcected error message   
+
+    present = Present()
+    with pytest.raises(Exception) as e:
+        present.unwrap()
+    error_message = str(e.value)
+    assert error_message == "No contents have been wrapped."    
+
+
+# def test_wrapped_once_and_unwrapped():
+#     # Make a new present object
+#     # Call wrap method and add contents
+#     # Call present.contents
+#     # Call unwrap method
+#     present = Present()
+#     present.wrap("Candy cane")
+#     print(present.contents())
+#     present.unwrap()
